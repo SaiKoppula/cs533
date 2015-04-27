9d06e2880401d4e2b5883fd907a49debe1b80518
Dao Lu <daoluhouston@gmail.com>
2015-04-26 19:57:47 -0500

    Added Dijkstra serial. Added a min heap implementation

---
+++ dijkstras/dijksra.cpp
@@ -0,0 +1,80 @@
+#include <pthread.h>
+#include <iostream>
+#include<lemon/graph_to_eps.h>
+#include <lemon/list_graph.h>
+#include <lemon/full_graph.h>
+#include <lemon/concepts/graph.h>
+#include <queue>
+#include <vector>
+#include <unordered_map>
+#include <pthread.h>
+#include <vector>
+#include "../lib/GraphGen.h"
+#include "../lib/support.h"
+#include "../lib/algorithms.h"
+
+#define ARGS 3
+#define TIMER_ENABLE 1
+#define PRINT_MST 0
+
+using namespace lemon;
+using namespace std;
+
+void usage()
+{
+    printf("USAGE: ./bfs [number of nodes] [number of threads]");
+}
+
+int main(int argc, char * argv[])
+{
+
+    //Param Check
+    if (argc != ARGS)
+    {
+        usage();
+        return -1;
+    }
+    
+    //typedef
+    typedef lemon::ListGraph Graph;
+ 	typedef Graph::EdgeIt EdgeIt;
+  	typedef Graph::Edge Edge;
+  	typedef Graph::NodeIt NodeIt;
+  	typedef Graph::Node Node;
+  	typedef Graph::EdgeMap<int> costMap;
+  	using lemon::INVALID;
+
+  	Graph g;
+  	
+  	int size;
+    int num_threads;
+	int num_edges=0;
+    size = atoi(argv[1]);
+    num_threads = atoi(argv[2]);
+    //Build graph -- Template uses Dao's Random Graph Generator
+    random_graph(&g, size, size/20, size/10, &num_edges);
+    #if PRINT_MST
+	cout << "NumEdges: "<< num_edges<<endl;
+	#endif
+	costMap c(g);
+	srand(time(NULL));
+	for(int i = 0; i < num_edges; i ++)
+	{
+		Edge e = Graph::edgeFromId(i);
+		c[e] = rand() % 90 + 5;
+		#if PRINT_MST
+		cout <<"("<< g.id(g.u(e))<<","<<g.id(g.v(e))<<")"<<",Value: "<<c[e]<<endl;
+		#endif
+	}
+	
+	vector<int> res_edge_id_serial;
+	vector<int> res_edge_id_parallel;
+	
+	/* Time the serial execution */
+	double t1 = get_clock();
+	for(int i = 0; i < size; i ++)
+	ourSerialDij(g, c, res_edge_id_serial, size, num_edges,1,i);
+	double t2 = get_clock();
+    printf("Serial Time: %lf seconds\n",(t2-t1));
+
+}
